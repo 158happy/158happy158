813dfabbcd9d790dca8977c245fcd818aa590447
158happy <68674461+158happy@users.noreply.github.com>
2020-07-25 00:40:20 +0800

    index.php       the one frist

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 <title>登陆</title>
 	</head>
 	<body>
-<form action="login.php" methon="get">
+<form action="login.php" methon="get">              //点击  “登陆” 用get方式提交表单form到 login.php 并跳转         点击 "注册"跳转到  register.php
 请输入信息登陆<br/>
 账号:<input type="text" name="name"><br />
 密码:<input type="password" name='psd'><br />
